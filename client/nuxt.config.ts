f8bdfc11c55a0fafbdb6aa29c474979445ae1690
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-08-22 15:16:06 +0200

    setup

---
+++ client/nuxt.config.ts
@@ -0,0 +1,36 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+	compatibilityDate: "2025-07-15",
+
+	devtools: { enabled: true },
+
+	modules: [
+		"@nuxt/eslint",
+		"@nuxt/fonts",
+		"@nuxt/icon",
+		"@nuxt/image",
+		"@nuxtjs/strapi",
+		"@pinia/nuxt",
+		"v-gsap-nuxt",
+		"nuxt-strapi-blocks-renderer",
+	],
+
+	typescript: {
+		typeCheck: true,
+	},
+
+	css: ["~/assets/css/main.css"],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	image: {
+		dir: "public/images",
+		strapi: {
+			baseURL: process.env.STRAPI_URL || "http://localhost:1337",
+		},
+	},
+});
+
