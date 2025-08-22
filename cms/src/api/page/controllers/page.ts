91d377d595cbf904b82bd5ad366307ad10efec38
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-08-22 14:39:48 +0200

    page

--- cms/src/api/page/controllers/page.ts
+++ cms/src/api/page/controllers/page.ts
@@ -10,6 +10,105 @@ export default factories.createCoreController("api::page.page", ({ strapi }) => 
 
 		const entity = await strapi.db.query("api::page.page").findOne({
 			where: { slug: id },
+			select: ["id", "title", "slug"],
+			populate: {
+				blocks: {
+					on: {
+						"block.hero": {
+							populate: {
+								select: ["__component", "id", "title", "colored_title", "description"],
+								cover: {
+									populate: {
+										select: ["formats", "name", "width", "height", "url", "provider"],
+									},
+								},
+								buttons: {
+									select: ["label", "url", "type", "variant"],
+								},
+							},
+						},
+						"block.list": {
+							populate: {
+								select: ["__component", "id", "title"],
+								button: {
+									select: ["label", "url", "type", "variant"],
+								},
+								items: {
+									select: ["title", "description"],
+								},
+							},
+						},
+						"block.info": {
+							populate: {
+								select: ["__component", "id", "title", "align_content"],
+								cover: {
+									populate: {
+										select: ["formats", "name", "width", "height", "url", "provider"],
+									},
+								},
+								button: {
+									select: ["label", "url", "type", "variant"],
+								},
+							},
+						},
+						"block.full-section": {
+							populate: {
+								select: ["__component", "id", "title", "description"],
+								cover: {
+									populate: {
+										select: ["formats", "name", "width", "height", "url", "provider"],
+									},
+								},
+								link: {
+									select: ["label", "url", "type", "variant"],
+								},
+							},
+						},
+						"block.faq": {
+							populate: {
+								select: ["__component", "id", "title"],
+								items: {
+									select: ["title", "description"],
+								},
+							},
+						},
+						"block.featured": {
+							populate: {
+								select: ["__component", "id", "title"],
+								services: {
+									select: ["title", "slug", "description"],
+									populate: {
+										icon: {
+											select: ["name", "has_image"],
+											populate: {
+												image: {
+													select: ["formats", "name", "width", "height", "url", "provider"],
+												},
+											},
+										},
+									},
+								},
+								articles: {
+									select: ["title", "slug", "description"],
+									populate: {
+										cover: {
+											select: ["formats", "name", "width", "height", "url", "provider"],
+										},
+									},
+								},
+								projects: {
+									select: ["title", "slug"],
+									populate: {
+										cover: {
+											select: ["formats", "name", "width", "height", "url", "provider"],
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
 		});
 
 		if (!entity) {
