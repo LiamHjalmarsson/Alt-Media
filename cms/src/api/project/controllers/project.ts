41db93ae0d42471dc48b77f5a4e0890e702968be
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-08-22 12:52:06 +0200

    Projects and services

--- cms/src/api/project/controllers/project.ts
+++ cms/src/api/project/controllers/project.ts
@@ -10,10 +10,59 @@ export default factories.createCoreController("api::project.project", ({ strapi 
 
 		const entity = await strapi.db.query("api::project.project").findOne({
 			where: { slug: id },
+			select: ["id", "title", "slug", "content"],
+			populate: {
+				cover: {
+					select: ["formats", "name", "width", "height", "url", "provider"],
+				},
+				services: {
+					select: ["title", "slug", "description", "content"],
+					populate: {
+						sub_services: {
+							select: ["title", "content"],
+							populate: {
+								tags: {
+									select: "title",
+								},
+							},
+						},
+						icon: {
+							select: ["name", "has_image"],
+							populate: {
+								image: {
+									select: ["formats", "name", "width", "height", "url", "provider"],
+								},
+							},
+						},
+					},
+				},
+			},
 		});
 
 		if (!entity) {
 			return ctx.notFound("Project not found");
+		}
+
+		const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+		return this.transformResponse(sanitizedEntity);
+	},
+
+	async find(ctx) {
+		const entity = await strapi.db.query("api::project.project").findMany({
+			select: ["id", "title", "slug"],
+			populate: {
+				cover: {
+					select: ["formats", "name", "width", "height", "url", "provider"],
+				},
+				services: {
+					select: ["title", "slug"],
+				},
+			},
+		});
+
+		if (!entity) {
+			return ctx.notFound("Projects not found");
 		}
 
 		const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
--- cms/src/api/service/controllers/service.ts
+++ cms/src/api/service/controllers/service.ts
@@ -10,10 +10,61 @@ export default factories.createCoreController("api::service.service", ({ strapi 
 
 		const entity = await strapi.db.query("api::service.service").findOne({
 			where: { slug: id },
+			select: ["id", "title", "slug", "content", "description"],
+			populate: {
+				sub_services: {
+					select: ["title", "content"],
+					populate: {
+						tags: {
+							select: "title",
+						},
+					},
+				},
+				icon: {
+					select: ["name", "has_image"],
+					populate: {
+						image: {
+							select: ["formats", "name", "width", "height", "url", "provider"],
+						},
+					},
+				},
+			},
 		});
 
 		if (!entity) {
 			return ctx.notFound("Service not found");
+		}
+
+		const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+		return this.transformResponse(sanitizedEntity);
+	},
+
+	async find(ctx) {
+		const entity = await strapi.db.query("api::service.service").findMany({
+			select: ["id", "title", "slug", "description", "content"],
+			populate: {
+				sub_services: {
+					select: ["title", "content"],
+					populate: {
+						tags: {
+							select: "title",
+						},
+					},
+				},
+				icon: {
+					select: ["name", "has_image"],
+					populate: {
+						image: {
+							select: ["formats", "name", "width", "height", "url", "provider"],
+						},
+					},
+				},
+			},
+		});
+
+		if (!entity) {
+			return ctx.notFound("Articles not found");
 		}
 
 		const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
